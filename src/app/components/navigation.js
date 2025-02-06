2912480ea039ce2741ef529bcb3c203464d9cf73
Lyudmil Ivanov <55487633+flmel@users.noreply.github.com>
2025-02-06 17:32:25 +0200

    init

---
+++ src/app/components/navigation.js
@@ -0,0 +1,30 @@
+"use client"
+import { useContext, useEffect, useState } from 'react';
+
+import { NearContext } from '@/wallets/near';
+
+export const Navigation = () => {
+  const { signedAccountId, wallet } = useContext(NearContext);
+  const [action, setAction] = useState(() => { });
+  const [label, setLabel] = useState('Loading...');
+
+  useEffect(() => {
+    if (!wallet) return;
+
+    if (signedAccountId) {
+      setAction(() => wallet.signOut);
+      setLabel(`Logout ${signedAccountId}`);
+    } else {
+      setAction(() => wallet.signIn);
+      setLabel('Login');
+    }
+  }, [signedAccountId, wallet]);
+
+  return (
+    <nav className="flex justify-end p-4">
+      <button className="rounded-full transition-colors bg-foreground text-background hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5  " onClick={action}>
+          {label}
+        </button>
+    </nav>
+  );
+};
